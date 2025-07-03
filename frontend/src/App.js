5b6cb0837317bae7bae6e341c6b45131af71f2ac
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-07-03 08:58:41 -0300

    Initial React App.js scaffold

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <h1>Rehearsal Scheduler</h1>
+      <p>Welcome – Schedule, communicate, and manage your music group here.</p>
+    </div>
+  );
+}
+export default App;
